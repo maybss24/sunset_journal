d9fc894a59f70652baeb254c17eb3ecf3dd2cb09
maybss24 <pesiganmaybel@gmail.com>
2025-06-18 09:56:30 +0800

    Activity 1

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('Sunset'),
+        backgroundColor: Colors.lime[300],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children:  [
+            Text('Text 1', style: TextStyle(fontSize: 20,
+              color: Colors.lime[700],
+              fontWeight: FontWeight.bold,
+              fontStyle: FontStyle.italic,)
+            ),
+            Text('Text 2', style: TextStyle(fontSize: 30,
+              color: Colors.lime[700],
+              fontWeight: FontWeight.bold,),
+             ),
+            Text('Text 3', style: TextStyle(fontSize: 40,
+              color: Colors.lime[700],
+              fontWeight: FontWeight.bold,),
+            ),
+          ],
+        ),
+      ),
+    ),
+  ));
+}
+
