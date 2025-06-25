bb96fb616b44938d39bb7f3c370fafcba5b79fbc
maybss24 <pesiganmaybel@gmail.com>
2025-06-25 09:57:35 +0800

    User Profile

--- lib/main.dart
+++ lib/main.dart
@@ -2,87 +2,180 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Scaffold(
+    home: Profile(),
+  ));
+}
+
+class Profile extends StatelessWidget {
+  const Profile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
         title: Text('Sunset'),
         backgroundColor: Colors.lime[300],
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Container(
-            margin: EdgeInsets.all(10),
-            padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10 ),
-          child: Row(
-              children: [
-                Text('Name:',
-                    style: TextStyle(
-                      fontSize: 20,
-                      color: Colors.lime,
-                      fontWeight: FontWeight.bold,
-                      fontStyle: FontStyle.italic,
-                    )
+          Center(
+            child: Padding(
+              padding: EdgeInsets.only(top: 30),
+              child: Text(
+                'User Profile',
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
                 ),
-                Text('Maybel Pesigan',
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      fontStyle: FontStyle.italic,
-                    )
-                ),
-              ]
+              ),
+            ),
           ),
-      ),
+
+          SizedBox(height: 20),
+
+          Center(
+            child: Icon(
+              Icons.person,
+              size: 100,
+            ),
+          ),
+
+          SizedBox(height: 20),
 
           Container(
             margin: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-            padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10 ),
-            child: Row(
-                children: [
-                  Text('Age:',
-                      style: TextStyle(
-                        fontSize: 20,
-                        color: Colors.lime,
-                        fontWeight: FontWeight.bold,
-                        fontStyle: FontStyle.italic,
-                      )
+            padding: EdgeInsets.symmetric(vertical: 5, horizontal: 30),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                Text(
+                  'Name:',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.lime,
+                    fontWeight: FontWeight.bold,
+                    fontStyle: FontStyle.italic,
                   ),
-                  Text('21 years old',
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        fontStyle: FontStyle.italic,
-                      )
+                ),
+                Text(
+                  'Maybel Pesigan',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    fontStyle: FontStyle.italic,
                   ),
-                ]
+                ),
+              ],
             ),
           ),
 
           Container(
-            margin: EdgeInsets.fromLTRB(5, 4, 3, 2,),
-            padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10 ),
-            child: Row(
-                children: [
-                  Text('Gender:',
-                      style: TextStyle(
-                        fontSize: 20,
-                        color: Colors.lime,
-                        fontWeight: FontWeight.bold,
-                        fontStyle: FontStyle.italic,
-                      )
+            margin: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+            padding: EdgeInsets.symmetric(vertical: 5, horizontal: 30),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                Text(
+                  'Email:',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.lime,
+                    fontWeight: FontWeight.bold,
+                    fontStyle: FontStyle.italic,
                   ),
-                  Text('Female',
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        fontStyle: FontStyle.italic,
-                      )
+                ),
+                Text(
+                  'pesiganmaybel@gmail.com',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    fontStyle: FontStyle.italic,
                   ),
-                ]
+                ),
+              ],
+            ),
+          ),
+
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+            padding: EdgeInsets.symmetric(vertical: 5, horizontal: 30),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                Text(
+                  'Company:',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.lime,
+                    fontWeight: FontWeight.bold,
+                    fontStyle: FontStyle.italic,
+                  ),
+                ),
+                Text(
+                  'Batangas State University TNEU Balayan',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    fontStyle: FontStyle.italic,
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          Container(
+            margin: EdgeInsets.fromLTRB(5, 4, 3, 2),
+            padding: EdgeInsets.symmetric(vertical: 5, horizontal: 30),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                Text(
+                  'Contact Number:',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.lime,
+                    fontWeight: FontWeight.bold,
+                    fontStyle: FontStyle.italic,
+                  ),
+                ),
+                Text(
+                  '09654189631',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    fontStyle: FontStyle.italic,
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          Center(
+            child: Padding(
+              padding: EdgeInsets.only(top: 35),
+              child: Container(
+                width: 350,
+                height: 45,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.lime[300],
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.logout),
+                      SizedBox(width: 10),
+                      Text('Logout'),
+                    ],
+                  ),
+                ),
+              ),
             ),
           ),
         ],
       ),
-    ),
-  ));
+    );
+  }
 }
-
